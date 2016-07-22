661685d18276da88accfe037fd0666da2557e1a8
qinmingyuan <mingyuan0715@foxmail.com>
2016-07-22 16:22:33 +0800

    fix export

--- lib/the_data/export.rb
+++ lib/the_data/export.rb
@@ -2,14 +2,19 @@ module TheData::Export
 
   def to_table
     @table_list = TableList.new(data_list_id: data_list_id)
-    @table_list.headers = header_result
+    @table_list.headers = header_result.to_csv
     @table_list.note_header = @note_header
     @table_list.note_footer = @note_footer
-    @table_list.footers = footer_result
+    @table_list.footers = footer_result.to_csv
     @table_list.save
 
+    to_table_items
+  end
+
+  def to_table_items
     collection_result.each_with_index do |object, index|
-      field_result(object, index)
+      row = field_result(object, index)
+      #@table_list.table_items.create(fields: row.to_csv)
     end
   end
 
@@ -23,7 +28,7 @@ module TheData::Export
         results << column[:header].titleize
       end
     end
-    results.to_csv
+    results
   end
 
   def footer_result
@@ -36,7 +41,7 @@ module TheData::Export
        results << column[:footer]
      end
     end
-    results.to_csv
+    results
   end
 
   def field_result(object, index)
@@ -49,8 +54,7 @@ module TheData::Export
       end
     end
 
-    row = CSV::Row.new(header_result, results)
-    @table_list.table_items.create(fields: row.to_csv)
+    CSV::Row.new(header_result, results)
   end
 
 end

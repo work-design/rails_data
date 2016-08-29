86ad334455f504a99946d68b8f390391e301e540
qinmingyuan <mingyuan0715@foxmail.com>
2016-08-29 14:33:05 +0800

    根据类型编辑信息

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,7 +1,8 @@
 class Conversion
   TYPE = {
-    date: :to_date,
-    integer: :to_i
+    date: { input: 'date', output: 'to_date' },
+    integer: { input: 'number', output: 'to_i' }
   }
+
 
 end

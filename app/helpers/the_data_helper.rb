68153df242a879f57aa153c51ffae8bde4e0e65c
qinmingyuan <mingyuan0715@foxmail.com>
2016-09-28 10:55:09 +0800

    not use self helper

--- app/helpers/the_data_helper.rb
+++ app/helpers/the_data_helper.rb
@@ -1,10 +1,10 @@
 module TheDataHelper
 
-  def simple_format_hash(hash_text, options = {})
+  def ex_format_hash(hash_text, options = {})
     wrapper_tag = options.fetch(:wrapper_tag, :p)
 
     hash_text.map do |k, v|
-      text = k.to_s + ': ' + v
+      text = k.to_s + ': ' + v.to_s
       content_tag(wrapper_tag, text)
     end.join("\n\n").html_safe
   end

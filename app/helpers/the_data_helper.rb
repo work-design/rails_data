b5dfd5956e66a0dd703ecd203cb5cb551c5af73a
qinmingyuan <mingyuan0715@foxmail.com>
2017-09-15 11:49:46 +0800

    push code

--- app/helpers/the_data_helper.rb
+++ app/helpers/the_data_helper.rb
@@ -1,19 +1,19 @@
 module TheDataHelper
-
-  def text_field_tag(name, value = nil, options = {})
-    type = TheData.config.mapping[options[:as].to_sym][:input]
-
-
-    if type == 'textarea'
-      return text_area_tag(name, value, options)
-    end
-
-    if type == 'select'
-      return select_tag name, options_for_select(TheData.config.mapping[options[:as].to_sym][:options]), options
-    end
-
-    options[:type] = type
-    super
-  end
+  #
+  # def text_field_tag(name, value = nil, options = {})
+  #   # type = TheData.config.mapping[options[:as].to_sym][:input]
+  #
+  #
+  #   if type == 'textarea'
+  #     return text_area_tag(name, value, options)
+  #   end
+  #
+  #   if type == 'select'
+  #     return select_tag name, options_for_select(TheData.config.mapping[options[:as].to_sym][:options]), options
+  #   end
+  #
+  #   # options[:type] = type
+  #   super
+  # end
 
 end

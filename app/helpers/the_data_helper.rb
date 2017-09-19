2ace834d638524d008aa5f68bce554fead5e5a1a
qinmingyuan <mingyuan0715@foxmail.com>
2017-09-19 17:06:18 +0800

    fix hidden tag helper

--- app/helpers/the_data_helper.rb
+++ app/helpers/the_data_helper.rb
@@ -3,21 +3,18 @@ module TheDataHelper
   def text_field_tag(name, value = nil, options = {})
     if options[:as]
       type = TheData.config.mapping[options[:as]][:input]
-    else
-      type = 'text'
-    end
 
-    if type == 'textarea'
-      return text_area_tag(name, value, options)
-    end
+      if type == 'textarea'
+        return text_area_tag(name, value, options)
+      end
 
-    if type == 'select'
-      opts = TheData.config.mapping[options[:as]][:options]
-      selected = TheData.config.mapping[options[:as]][:selected]
-      return select_tag name, options_for_select(opts, selected), options
+      if type == 'select'
+        opts = TheData.config.mapping[options[:as]][:options]
+        selected = TheData.config.mapping[options[:as]][:selected]
+        return select_tag name, options_for_select(opts, selected), options
+      end
     end
-
-    options[:type] = type
+    
     super
   end
 

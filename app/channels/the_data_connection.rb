05ef228e4fbdfc3154182fd3dd1df15cd42a88fe
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-23 12:07:12 +0800

    fix the data connection

--- app/channels/the_data_connection.rb
+++ app/channels/the_data_connection.rb
@@ -2,26 +2,21 @@
 module TheDataConnection
 
   def self.prepended(model)
-    model.identified_by :current_user_id
+    model.identified_by :current_user
   end
 
   def connect
-    self.current_user_id = session['user_id']
+    self.current_user = find_verified_user
     super
   end
 
   protected
   def find_verified_user
     if session['user_id']
-      current_user = User.find_by id: session['user_id']
+      User.find_by id: session['user_id']
     else
-      current_user = nil
-    end
-
-    if current_user
-      current_user
-    else
-      reject_unauthorized_connection
+      logger.error 'An unauthorized connection attempt was rejected'
+      nil
     end
   end
 

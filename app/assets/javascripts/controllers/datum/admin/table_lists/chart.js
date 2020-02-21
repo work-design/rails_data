384161793c2057bbf952201c483f07ec00056ad7
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-21 18:40:54 +0800

    test fetch works

--- app/assets/javascripts/controllers/datum/admin/table_lists/chart.js
+++ app/assets/javascripts/controllers/datum/admin/table_lists/chart.js
@@ -6,7 +6,7 @@ url.pathname = url.pathname.replace(/\/chart$/, '')
 url.searchParams.set('columns', chart.dataset.columns)
 fetch(url, {
   headers: {
-    'Content-Type': 'application/json'
+    'Accept': 'application/json'
   }
 }).then((res) => res.json()).then((data) => {
   const linePlot = new Line('chart', {

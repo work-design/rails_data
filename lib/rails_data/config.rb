9845f45fc29e6e28f6dac773a1115b64a171dbd4
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-18 10:52:53 +0800

    move config to rails com

--- lib/rails_data/config.rb
+++ lib/rails_data/config.rb
@@ -4,19 +4,11 @@ module RailsData
   # Config
   # Configure the standard CSV default options
   # as well the option to output the header row
-
   include ActiveSupport::Configurable
 
   configure do |config|
-    config.mapping = ActiveSupport::OrderedOptions.new
-
     config.inflector = :titleize
     config.method_name = :report
-    config.mapping.date = { input: 'date', output: 'to_date' }
-    config.mapping.integer = { input: 'number', output: 'to_i' }
-    config.mapping.string = { input: 'text', output: 'to_s' }
-    config.mapping.text = { input: 'textarea', output: 'to_s' }
-    config.mapping.array = { input: 'array', output: 'to_s' }
   end
 
 end

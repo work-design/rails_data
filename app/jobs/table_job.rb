ddcbd25596dca12b797f6d2082691a45b95f88ea
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-21 22:39:58 +0800

    find

--- app/jobs/table_job.rb
+++ app/jobs/table_job.rb
@@ -1,11 +1,13 @@
-class TableJob < ApplicationJob
-  queue_as :default
+module Datum
+  class TableJob < ApplicationJob
+    queue_as :default
 
-  def perform(table_list_id, user_id)
-    @table_list = TableList.find(table_list_id)
-    @table_list.run
+    def perform(table_list_id, user_id)
+      @table_list = TableList.find(table_list_id)
+      @table_list.run
 
-    ActionCable.server.broadcast "user:#{user_id}", body: '<i class="green checkmark icon"></i>', done_id: table_list_id
+      ActionCable.server.broadcast "user:#{user_id}", body: '<i class="green checkmark icon"></i>', done_id: table_list_id
+    end
+
   end
-
 end

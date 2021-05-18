750f7ef73cbffc0ede075977c1805b85f6b52b4e
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-18 18:14:54 +0800

    datum

---
+++ app/models/datum.rb
@@ -0,0 +1,11 @@
+module Datum
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'datum_'
+  end
+
+end
--- lib/rails_data.rb
+++ lib/rails_data.rb
@@ -3,11 +3,3 @@ require 'rails_data/config'
 require 'rails_data/export'
 require 'rails_data/import'
 require 'rails_data/record'
-
-module Datum
-
-  def self.use_relative_model_naming?
-    true
-  end
-
-end

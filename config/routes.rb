628b1a3df25097f20973763b0220035d2b9b6718
MingyuanQin <mingyuan0715@foxmail.com>
2021-07-17 14:33:38 +0800

    fix many pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,44 @@
 Rails.application.routes.draw do
 
-  scope :panel, module: 'datum/panel', defaults: { business: 'datum', namespace: 'panel' } do
-    resources :data_lists do
-      member do
-        put :update_publish
-        patch :rebuild
+  namespace :datum, defaults: { business: 'datum' } do
+    namespace :panel, defaults: { namespace: 'panel' } do
+      resources :data_lists do
+        member do
+          put :update_publish
+          patch :rebuild
+        end
+        resources :table_lists do
+          collection do
+            get :find
+            get :direct
+            get 'import' => :new_import
+            post 'import' => :create_import
+          end
+          member do
+            get :chart
+            get :row
+            get :xlsx
+            patch :run
+            patch :migrate
+          end
+        end
       end
-      resources :table_lists do
+      resources :data_records do
         collection do
-          get :find
-          get :direct
-          get 'import' => :new_import
-          post 'import' => :create_import
+          get :add_item
+          get :remove_item
         end
         member do
-          get :chart
-          get :row
-          get :xlsx
-          patch :run
-          patch :migrate
+          patch :rebuild
         end
-      end
-    end
-
-    resources :data_records do
-      collection do
-        get :add_item
-        get :remove_item
-      end
-      member do
-        patch :rebuild
-      end
-      resources :record_lists do
-        get :find, on: :collection
-        member do
-          get :row
-          patch :run
-          get 'columns' => :edit_columns
-          patch 'columns' => :update_columns
+        resources :record_lists do
+          get :find, on: :collection
+          member do
+            get :row
+            patch :run
+            get 'columns' => :edit_columns
+            patch 'columns' => :update_columns
+          end
         end
       end
     end

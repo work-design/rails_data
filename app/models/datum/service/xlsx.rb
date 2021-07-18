db5556972760ac511f77e81224b3ae3f0c9ab07d
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-18 17:04:49 +0800

    xlsx common

--- app/models/datum/service/xlsx.rb
+++ app/models/datum/service/xlsx.rb
@@ -2,14 +2,14 @@ require 'write_xlsx'
 module Datum
   class Service::Xlsx
     include DataExportHelper
-    attr_reader :sheet, :table_list, :params, :headers
+    attr_reader :sheet, :table_list, :params, :headers, :config_table
 
-    def initialize(table_list:, params: {}, headers: [])
+    def initialize(table_list:, params: table_list.convert_parameters, headers: table_list.headers, config_table: @data_list.config_table)
       @table_list = table_list
       @data_list = table_list.data_list
-      @headers = table_list.headers
-      @table_list.convert_parameters
-      @config_table = @data_list.config_table
+      @headers = headers
+      @params = params
+      @config_table = config_table
 
       @io = StringIO.new
       @workbook = WriteXLSX.new(@io)
@@ -19,7 +19,7 @@ module Datum
     def direct_xlsx
       sheet.write_row(0, 0, headers)
 
-      @config_table.collection.call(@params).each_with_index do |object, index|
+      config_table.collection.call(params).each_with_index do |object, index|
         row = field_result(object, index)
         sheet.write_row(index + 1, 0, row)
       end

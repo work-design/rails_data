1c67271d69d1ec8536e0b1f8c05e96706d5d5b56
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-25 19:58:15 +0800

    rename test file

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -3,8 +3,5 @@ require 'test_helper'
 class NavigationTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # test "the truth" do
-  #   assert true
-  # end
 end
 
--- test/models/data_list_test.rb
+++ test/models/data_list_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 class DataListTest < ActiveSupport::TestCase
 
-
   test 'create_model' do
     report_list = create :data_list
   end

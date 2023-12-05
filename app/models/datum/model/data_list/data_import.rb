ca0cda35b1157252d0f525dd7e7c3b14d84074de
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-05 15:20:00 +0800

    data importe

--- app/models/datum/model/data_list/data_import.rb
+++ app/models/datum/model/data_list/data_import.rb
@@ -1,5 +1,22 @@
+require 'write_xlsx'
 module Datum
   module Model::DataList::DataImport
+    extend ActiveSupport::Concern
+
+    def template
+      io = StringIO.new
+      workbook = WriteXLSX.new(io)
+
+      sheet = workbook.add_worksheet
+      sheet.write_row(0, 0, headers)
+
+      workbook.close
+      io.string
+    end
+
+    def headers
+      export.columns.values.map(&->(i){ i[:header] })
+    end
 
   end
 end

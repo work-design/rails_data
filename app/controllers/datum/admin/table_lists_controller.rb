1a2e9b0a2e8259639586bd0c0abb28e5c8896ac0
qinmingyuan <mingyuan0715@foxmail.com>
2024-06-12 15:20:02 +0800

    new import

--- app/controllers/datum/admin/table_lists_controller.rb
+++ app/controllers/datum/admin/table_lists_controller.rb
@@ -2,7 +2,7 @@ module Datum
   class Admin::TableListsController < Panel::TableListsController
     include Controller::Admin
     before_action :set_data_list
-    before_action :set_table_list
+    before_action :set_table_list, except: [:new_import, :create_import]
 
     def find
       q_params = {}

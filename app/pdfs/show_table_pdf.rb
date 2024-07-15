cc427e3ce8b33a92fc7cc352c5dc12f527c3f11c
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-15 18:17:40 +0800

    show items

--- app/pdfs/show_table_pdf.rb
+++ app/pdfs/show_table_pdf.rb
@@ -29,7 +29,7 @@ class ShowTablePdf < TablePdf
       }
     }
     undash
-    _data = data.flatten.in_groups_of(4)
+    _data = data.flatten(1).in_groups_of(4)
     table(_data, options) do
       columns(0).style NORMAL_TH
       columns(1).style NORMAL_TD

bfd5e6c7cb8d69e591b143d95a8ff2fb880b431b
qinmingyuan <mingyuan0715@foxmail.com>
2024-08-11 18:04:46 +0800

    image size

--- app/pdfs/tables/image.rb
+++ app/pdfs/tables/image.rb
@@ -9,13 +9,20 @@ module Tables::Image
   def image_table(data, **options)
     images = data.values[0]
     return if images.blank?
-    width = bounds.width / images.size
+    if images.size <= 2
+      size = 2
+    elsif images.size >= 4
+      size = 4
+    else
+      size = images.size
+    end
+    width = bounds.width / size
     _data = [images.map { |i| i.merge! image_width: width, position: :center, vposition: :bottom }]
 
     default_options = {
       position: :center,
       width: bounds.width,
-      column_widths: Array.new(images.size, width),
+      column_widths: Array.new(size, width),
       cell_style: {
         borders: [],
         padding: [5, 0],

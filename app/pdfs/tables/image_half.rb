7784958e47d2d9ee968d7033c0c04086a696ef34
qinmingyuan <mingyuan0715@foxmail.com>
2024-08-19 14:27:12 +0800

    update rails

--- app/pdfs/tables/image_half.rb
+++ app/pdfs/tables/image_half.rb
@@ -9,24 +9,13 @@ module Tables::ImageHalf
   def image_half_table(data, **options)
     images = data.values[0]
     return if images.blank?
-    if images.size <= 2
-      size = 2
-    elsif images.size >= 4
-      size = 4
-    else
-      size = images.size
-    end
-    width = bounds.width / size
-    if size > images.size
-      _data = [(images.map { |i| i.merge! image_width: width, position: :center, vposition: :bottom }).concat(Array.new(size - images.size, nil))]
-    else
-      _data = [images.map { |i| i.merge! image_width: width, position: :center, vposition: :bottom }]
-    end
+    width = bounds.width / 2
+    _data = [images.map { |i| i.merge! image_width: width, position: :center, vposition: :bottom }]
 
     default_options = {
-      position: :center,
-      width: bounds.width,
-      column_widths: Array.new(size, width),
+      position: :right,
+      width: width,
+      column_widths: [width],
       cell_style: {
         borders: [],
         padding: [5, 0],

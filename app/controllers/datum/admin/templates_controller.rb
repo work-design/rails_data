00707256f2618de238b9b549773b96b83d12f23f
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-07 19:52:27 +0800

    template params

--- app/controllers/datum/admin/templates_controller.rb
+++ app/controllers/datum/admin/templates_controller.rb
@@ -1,5 +1,16 @@
 module Datum
   class Admin::TemplatesController < Admin::BaseController
 
+    private
+    def template_params
+      result = params.fetch(:template, {}).permit(
+        :name,
+        :file,
+        parameters: {}
+      )
+      result['parameters'] = result['parameters'].values.each_with_object({}) { |i, h|  h.merge! i['column'] => i['value'] }
+      result
+    end
+
   end
 end

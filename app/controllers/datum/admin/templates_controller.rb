456a1b26191a7cd3059595a05bada6baa6301649
xulin <flyxcat@outlook.com>
2024-12-18 10:21:26 +0800

    d

--- app/controllers/datum/admin/templates_controller.rb
+++ app/controllers/datum/admin/templates_controller.rb
@@ -1,7 +1,17 @@
 module Datum
   class Admin::TemplatesController < Admin::BaseController
+    before_action :set_new_template, only: [:new, :create]
+    before_action :set_template, only: [:show, :edit, :update, :destroy]
 
     private
+    def set_new_template
+      @template = Template.new(template_params)
+    end
+
+    def set_template
+      @template = Template.find params[:id]
+    end
+
     def template_params
       result = params.fetch(:template, {}).permit(
         :name,

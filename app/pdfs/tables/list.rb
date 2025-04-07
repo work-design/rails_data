f27d7cf8d0bba62936c67dbdae1f7d380fb3fd1e
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-07 20:58:14 +0800

    fixer

--- app/pdfs/tables/list.rb
+++ app/pdfs/tables/list.rb
@@ -3,9 +3,10 @@
 module Tables::List
 
   def list_table(data, **options)
-    return if data.blank?
-
-    undash
+    stroke_color 'eeeeee'
+    stroke do
+      horizontal_rule
+    end
     data.each do |i|
       move_down 25
       text i, inline_format: true
